3b64c41b6d06df1678e8484215bdeb707c5c1d56
HyP3r10Nn <hyp3rr10nn@gmail.com>
2025-06-09 21:46:09 +0200

    passgen.py

--- passgen.py
+++ passgen.py
@@ -9,14 +9,14 @@ all_chars = numbers + Bchars + schars + symbols
 
 while True:
     try:
-        num = int(input("Zadej délku hesla: "))
+        num = int(input("input length of your password: "))
         if num <= 0:
-            print("Délka musí být větší než 0.")
+            print("password length must be bigger than 0.")
         else:
             break
     except ValueError:
-        print("Zadej celé číslo!")
+        print("please input number.")
 
 password = "".join(random.choice(all_chars) for _ in range(num))
 
-print("Vygenerované heslo:", password)
+print("your generated password: ", password)
